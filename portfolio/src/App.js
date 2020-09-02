ec3a502c0ed07aaae5326d42e4b684d5c6921898
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-02 18:32:39 -0500

    Building Home Component.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import Home from './components/pages/Home'
-import Projects from './components/pages/Projects'
-import About from './components/pages/About'
+
 
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio Coming Soon... please stand by!</h1>
       <Home />
-      <Projects />
-      <About />
     </div>
   );
 }
--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -1,11 +1,36 @@
 import React, {Component} from "react";
 import '../../styles/Home.css'
+import logo from '../../components/logo.png';
 
 class Home extends Component {
     render() {
         return (
             <div className="Home">
-                <h1>Home Page Component</h1>
+                
+                <header id="logo">
+                    
+                        <img src={logo} width="150" height="150" alt="logo"></img>
+                    
+                </header>
+
+
+                <div id="projects">
+                       
+                        <h1>Projects</h1>
+                    
+                </div>
+
+                <div id="about">
+                        
+                        <h1>About</h1>
+                    
+                </div>
+
+
+                <div id="icons">
+                    
+                        
+                </div>
             </div>
         )
     }
