a1b60c285482190772c380c7cdd68e771da08ba8
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-03 15:49:02 -0500

    Installed and Imported FontAwesome, Google Fonts, made home page styling and animations.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import Home from './components/pages/Home'
+import Home from './components/pages/Home';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faEnvelope)
 
 
 
--- portfolio/src/components/pages/Home.js
+++ portfolio/src/components/pages/Home.js
@@ -1,36 +1,32 @@
 import React, {Component} from "react";
 import '../../styles/Home.css'
 import logo from '../../components/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 class Home extends Component {
     render() {
         return (
             <div className="Home">
                 
-                <header id="logo">
-                    
-                        <img src={logo} width="150" height="150" alt="logo"></img>
-                    
+                <header className="logo">
+                    <img src={logo} width="150" height="150" alt="logo"></img>
                 </header>
 
-
-                <div id="projects">
-                       
-                        <h1>Projects</h1>
-                    
+                <div className="projects">               
+                    <h1>Projects</h1>    
                 </div>
 
-                <div id="about">
-                        
-                        <h1>About</h1>
-                    
+                <div className="about">
+                    <h1>About</h1>
                 </div>
 
-
-                <div id="icons">
-                    
-                        
+                <div className="icons">
+                    <FontAwesomeIcon className="linkedInBtn" icon={['fab', 'linkedin']} size= "2x" />
+                    <FontAwesomeIcon className="gitHubBtn" icon={['fab', 'github']} size="2x" />
+                    <FontAwesomeIcon className="emailBtn" icon="envelope" size="2x" />
                 </div>
+            
             </div>
         )
     }
