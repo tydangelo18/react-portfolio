2654c371602696a192847f609df6213c81f2cc02
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-05 16:41:09 -0500

    Revert "Menu Components."
    This reverts commit 7e50401af552ee0b499c164e26fa01055f415ea0.

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,13 +1,12 @@
 import React, {Component} from "react";
 import '../styles/Projects.css'
 import ProjectCards from '../components/ProjectCards'
-import Menu from '../components/Menu';
 
 class Projects extends Component {
     render() {
         return (
             <div>
-                <Menu />
+        
                 <ProjectCards />
                 <ProjectCards />
             </div>
