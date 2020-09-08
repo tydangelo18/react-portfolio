4dcad5ca1b02c085afbe657ce65f84a68cc361f2
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-07 22:26:18 -0500

    Updates

---
+++ precache-manifest.9c9db57d29607518bf42a17885fccf4b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3f9f0162be01607df40efd20de63c9b",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "07de73ef7cb2afd15a66",
+    "url": "/react-portfolio/static/css/main.a642be22.chunk.css"
+  },
+  {
+    "revision": "442ba7544c52ddd35eb3",
+    "url": "/react-portfolio/static/js/2.3807005f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.3807005f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07de73ef7cb2afd15a66",
+    "url": "/react-portfolio/static/js/main.21ebbe73.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "179aa8da0e6b0f479d0add011b2b5783",
+    "url": "/react-portfolio/static/media/Resume.179aa8da.pdf"
+  },
+  {
+    "revision": "adec844f58fbf0ff10b4c78a54f82f0d",
+    "url": "/react-portfolio/static/media/burger.adec844f.jpg"
+  },
+  {
+    "revision": "1471ff30cd0479a32de5fe048042c77e",
+    "url": "/react-portfolio/static/media/filter.1471ff30.jpg"
+  },
+  {
+    "revision": "61f5471ceddfd7a0861159fca0fca048",
+    "url": "/react-portfolio/static/media/logo.61f5471c.png"
+  },
+  {
+    "revision": "d41b45ec72729ba92cd5c54fb15b0f85",
+    "url": "/react-portfolio/static/media/password.d41b45ec.jpg"
+  },
+  {
+    "revision": "9717b209ee9e352d0e67d58ea42c2116",
+    "url": "/react-portfolio/static/media/planner.9717b209.jpg"
+  },
+  {
+    "revision": "c330cd17f45f7af385934f8dc7fca407",
+    "url": "/react-portfolio/static/media/readme.c330cd17.jpg"
+  },
+  {
+    "revision": "5caf2c149cfb4113b879d7a75a4ced45",
+    "url": "/react-portfolio/static/media/springboard.5caf2c14.jpg"
+  }
+]);
