c99de5ddc82516d84f2aed787e0d532cecd8dbb1
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-07 21:54:05 -0500

    Animations.

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -7,23 +7,23 @@ class Header extends Component {
     render() {
         return (
             <header className="header">
-                <nav className="header-navigation">
-                    <div className="header-button">
-                        <SideBarButton click={this.props.barClickHandler} />
-                    </div>
-                    <div className="header-logo">
-                    <a href="/">
-                        <img src={logo} width="50" height="50" alt="logo"></img>
-                    </a></div>
-                    <div className="spacer" />
-                    <div className="header-navigation-items">
-                        <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/projects">Projects</a></li>
-                            <li><a href="/about">About</a></li>
-                        </ul>
-                    </div>
-                </nav>
+                    <nav className="header-navigation">
+                        <div className="header-button">
+                            <SideBarButton click={this.props.barClickHandler} />
+                        </div>
+                        <div className="header-logo">
+                        <a href="/">
+                            <img src={logo} width="50" height="50" alt="logo"></img>
+                        </a></div>
+                        <div className="spacer" />
+                        <div className="header-navigation-items">
+                            <ul>
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/projects">Projects</a></li>
+                                <li><a href="/about">About</a></li>
+                            </ul>
+                        </div>
+                    </nav>
             </header>
         )
     }
