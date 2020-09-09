865bfc437ea21b26f746893bfbade55a9c142c1a
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-08 20:59:41 -0500

    Fixed icon links.

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import '../styles/Icons.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Mailto from '../components/Mailto';
@@ -14,12 +13,12 @@ class Icons extends Component {
 
         return (
             <div className="icons">
-                <Link style={btnStyle} to={{ pathname: "https://www.linkedin.com/in/tydangelo18/" }} target="null">
+                <a style={btnStyle} href="https://www.linkedin.com/in/tydangelo18/" target="null">
                         <FontAwesomeIcon className="linkedInBtn" icon={['fab', 'linkedin']} size= "2x" />
-                    </Link>
-                    <Link style={btnStyle} to={{ pathname: "https://github.com/tydangelo18" }} target="null">
+                    </a>
+                    <a style={btnStyle} href="https://github.com/tydangelo18" target="null">
                         <FontAwesomeIcon className="gitHubBtn" icon={['fab', 'github']} size="2x" />
-                    </Link>
+                    </a>
                     
                     <Mailto email="tydangelo18@gmail.com">
                         <FontAwesomeIcon style={btnStyle} className="emailBtn" icon="envelope" size="2x" />
