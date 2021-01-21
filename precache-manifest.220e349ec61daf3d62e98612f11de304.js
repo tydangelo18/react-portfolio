f10bafa20c2e9a6a2bc562dc2d95ab7e59e9d7fd
Tyler D'Angelo <tydangelo18@gmail.com>
2021-01-20 22:04:10 -0600

    Update precache-manifest.220e349ec61daf3d62e98612f11de304.js

--- precache-manifest.220e349ec61daf3d62e98612f11de304.js
+++ precache-manifest.220e349ec61daf3d62e98612f11de304.js
@@ -1,58 +1,58 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "922f64d701c2b1a4238e602c2cb8ab97",
-    "url": "/react-portfolio/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "0e488520c422145f8959",
-    "url": "/react-portfolio/static/css/main.a642be22.chunk.css"
+    "url": "/static/css/main.a642be22.chunk.css"
   },
   {
     "revision": "01210dea0c489c84ca1a",
-    "url": "/react-portfolio/static/js/2.7c5221b0.chunk.js"
+    "url": "/static/js/2.7c5221b0.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react-portfolio/static/js/2.7c5221b0.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.7c5221b0.chunk.js.LICENSE.txt"
   },
   {
     "revision": "0e488520c422145f8959",
-    "url": "/react-portfolio/static/js/main.71d7158f.chunk.js"
+    "url": "/static/js/main.71d7158f.chunk.js"
   },
   {
     "revision": "73a871a578ef5a2aeb49",
-    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+    "url": "/static/js/runtime-main.1d992053.js"
   },
   {
     "revision": "1418e42a4faf46007fee91477c156d3f",
-    "url": "/react-portfolio/static/media/Resume.1418e42a.pdf"
+    "url": "/static/media/Resume.1418e42a.pdf"
   },
   {
     "revision": "adec844f58fbf0ff10b4c78a54f82f0d",
-    "url": "/react-portfolio/static/media/burger.adec844f.jpg"
+    "url": "/static/media/burger.adec844f.jpg"
   },
   {
     "revision": "1471ff30cd0479a32de5fe048042c77e",
-    "url": "/react-portfolio/static/media/filter.1471ff30.jpg"
+    "url": "/static/media/filter.1471ff30.jpg"
   },
   {
     "revision": "61f5471ceddfd7a0861159fca0fca048",
-    "url": "/react-portfolio/static/media/logo.61f5471c.png"
+    "url": "/static/media/logo.61f5471c.png"
   },
   {
     "revision": "9717b209ee9e352d0e67d58ea42c2116",
-    "url": "/react-portfolio/static/media/planner.9717b209.jpg"
+    "url": "/static/media/planner.9717b209.jpg"
   },
   {
     "revision": "c330cd17f45f7af385934f8dc7fca407",
-    "url": "/react-portfolio/static/media/readme.c330cd17.jpg"
+    "url": "/static/media/readme.c330cd17.jpg"
   },
   {
     "revision": "5caf2c149cfb4113b879d7a75a4ced45",
-    "url": "/react-portfolio/static/media/springboard.5caf2c14.jpg"
+    "url": "/static/media/springboard.5caf2c14.jpg"
   },
   {
     "revision": "42b1de7ff90fe881c02b6c71349d22c5",
-    "url": "/react-portfolio/static/media/weather.42b1de7f.jpg"
+    "url": "/static/media/weather.42b1de7f.jpg"
   }
 ]);
