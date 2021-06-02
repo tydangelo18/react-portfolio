593c4512189ab88ffed3c3b9960b52a428fd486c
Ty D'Angelo <tydangelo18@gmail.com>
2021-06-01 21:22:18 -0500

    Project Cards update. Added in bol and finch.

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import '../styles/ProjectCards.css';
-import weather from '../utilities/weather.jpg';
-import planner from '../utilities/planner.jpg';
+import bol from '../utilities/bol.jpg';
+import landing from '../utilities/landing.jpg';
 import springboard from '../utilities/springboard.jpg';
 import photography from '../utilities/photography.jpg';
 import burger from '../utilities/burger.jpg';
-import filter from '../utilities/filter.jpg';
+import finch from '../utilities/finch.jpg';
 
 class ProjectCards extends Component {
   render() {
@@ -14,6 +14,17 @@ class ProjectCards extends Component {
         <div className='cardDiv'>
           <div className='topRow'>
             <div className='container' id='springboardCard'>
+              <img src={bol} alt='bol' className='image'></img>
+              <div className='overlay'>
+                <a
+                  href='https://calm-thicket-62947.herokuapp.com/'
+                  target='null'
+                >
+                  <div className='text'>bol</div>
+                </a>
+              </div>
+            </div>
+            <div className='container' id='passwordCard'>
               <img src={springboard} alt='Springboard' className='image'></img>
               <div className='overlay'>
                 <a
@@ -24,42 +35,20 @@ class ProjectCards extends Component {
                 </a>
               </div>
             </div>
-            <div className='container' id='passwordCard'>
-              <img src={weather} alt='password' className='image'></img>
-              <div className='overlay'>
-                <a
-                  href='https://tydangelo18.github.io/weatherdashboard/'
-                  target='null'
-                >
-                  <div className='text'>Weather Dashboard</div>
-                </a>
-              </div>
-            </div>
             <div className='container' id='burgerCard'>
-              <img src={burger} alt='Burger Logger' className='image'></img>
+              <img src={finch} alt='Finch' className='image'></img>
               <div className='overlay'>
                 <a
-                  href='https://lit-thicket-35467.herokuapp.com/'
+                  href='https://hidden-savannah-51942.herokuapp.com'
                   target='null'
                 >
-                  <div className='text'>Burger Logger</div>
+                  <div className='text'>Finch</div>
                 </a>
               </div>
             </div>
           </div>
           <div className='bottomRow'>
             <div className='container' id='plannerCard'>
-              <img src={planner} alt='Web Organizer' className='image'></img>
-              <div className='overlay'>
-                <a
-                  href='https://floating-garden-26225.herokuapp.com/'
-                  target='null'
-                >
-                  <div className='text'>Web Organizer</div>
-                </a>
-              </div>
-            </div>
-            <div className='container' id='readmeCard'>
               <img
                 src={photography}
                 alt='Cody J. Brown Photography'
@@ -74,15 +63,23 @@ class ProjectCards extends Component {
                 </a>
               </div>
             </div>
-
-            <div className='container' id='filterCard'>
-              <img src={filter} alt='Landing Page' className='image'></img>
+            <div className='container' id='readmeCard'>
+              <img src={landing} alt='Landing Page' className='image'></img>
               <div className='overlay'>
                 <a
                   href='https://tydangelo18.github.io/landingpage/'
                   target='null'
                 >
                   <div className='text'>Landing Page</div>
+                </a>
+              </div>
+            </div>
+
+            <div className='container' id='filterCard'>
+              <img src={burger} alt='We Love Burgers' className='image'></img>
+              <div className='overlay'>
+                <a href='https://lit-thicket-35467.herokuapp.com' target='null'>
+                  <div className='text'>We Love Burgers</div>
                 </a>
               </div>
             </div>
